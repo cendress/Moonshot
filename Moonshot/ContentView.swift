b2c4dfd157d44621dbdd2a07d10108cfb927fb6f
cendress <95492688+cendress@users.noreply.github.com>
2023-11-22 11:06:09 -0500

    Implement mission view

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,17 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+  let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+  let missions: [Mission] = Bundle.main.decode("missions.json")
+  
+  let columns = [
+    GridItem(.adaptive(minimum: 150))
+  ]
+  
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    NavigationView {
+      ScrollView {
+        LazyVGrid(columns: columns) {
+          ForEach(missions) { mission in
+            NavigationLink {
+              MissionView(mission: mission, astronauts: astronauts)
+            } label: {
+              VStack {
+                Image(mission.image)
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 100, height: 100)
+                  .padding()
+                
+                VStack {
+                  Text(mission.displayName)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                  
+                  Text(mission.formattedLaunchDate)
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.5))
+                }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity)
+                .background(.lightBackground)
+              }
+              .clipShape(RoundedRectangle(cornerRadius: 10))
+              .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                  .stroke(.lightBackground)
+              )
+            }
+          }
+        }
+        .padding([.horizontal, .bottom])
+      }
+      .navigationTitle("Moonshot")
+      .background(.darkBackground)
+      .preferredColorScheme(.dark)
     }
-    .padding()
   }
 }
 
-#Preview {
-  ContentView()
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
 }
