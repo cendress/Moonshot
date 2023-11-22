c9da16fad227a586b096a46d3ba02cfc98b5d682
cendress <95492688+cendress@users.noreply.github.com>
2023-11-22 10:59:37 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Christopher Endress on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
